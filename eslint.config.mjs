31d7784d4f6f6de56036c8bde6b7a9483244afa7
WojciechSz <w.szczygielski00@gmail.com>
2025-03-05 13:42:16 +0100

    styles

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,43 +14,43 @@ const compat = new FlatCompat({
 
 const config = [
   {
-    ignores: ['src/components/ui/**/*'],
+    ignores: ["src/components/ui/**/*"],
   },
   ...compat.extends(
-    'next/core-web-vitals',
-    'next/typescript',
-    'standard',
-    'plugin:tailwindcss/recommended',
-    'prettier'
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    // "plugin:tailwindcss/recommended",
+    "prettier"
   ),
   {
     rules: {
-      'import/order': [
-        'error',
+      "import/order": [
+        "error",
         {
           groups: [
-            'builtin',
-            'external',
-            'internal',
-            ['parent', 'sibling'],
-            'index',
-            'object',
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
           ],
 
-          'newlines-between': 'always',
+          "newlines-between": "always",
 
           pathGroups: [
             {
-              pattern: '@app/**',
-              group: 'external',
-              position: 'after',
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
             },
           ],
 
-          pathGroupsExcludedImportTypes: ['builtin'],
+          pathGroupsExcludedImportTypes: ["builtin"],
 
           alphabetize: {
-            order: 'asc',
+            order: "asc",
             caseInsensitive: true,
           },
         },
@@ -58,10 +58,10 @@ const config = [
     },
   },
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ["**/*.ts", "**/*.tsx"],
 
     rules: {
-      'no-undef': 'off',
+      "no-undef": "off",
     },
   },
 ];
